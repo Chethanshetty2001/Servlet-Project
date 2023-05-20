225c4be59794f496a9c15686c7dda84efa1fbe18
Chethan S <129203864+Chethanshetty2001@users.noreply.github.com>
2023-05-21 00:08:16 +0530

    Update print.java

--- java/controller/print.java
+++ java/controller/print.java
@@ -18,26 +18,6 @@ public class print extends GenericServlet{
 
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		String name = req.getParameter("name");
-//		long MobileNo = Long.parseLong(req.getParameter("mobile"));
-//		String Email = req.getParameter("mail");
-//		String Gender = req.getParameter("gender");
-//		Date DOB = Date.valueOf(req.getParameter("dob"));
-//		String country = req.getParameter("country");
-//		String[] skills = req.getParameterValues("skills");
-//		String SQL = (req.getParameter("SQL")!=null?"sql":"");
-//		String JDBC = (req.getParameter("JDBC")!=null?"JDBC":"");
-//		String Hibernate =( req.getParameter("Hibernate")!=null?"hibernate":"");
-//		String Address = req.getParameter("address");
-//		res.getWriter().print("<h1>"+"Name:"+name
-//				+"<br> MobileNo:"+MobileNo
-//				+"<br> Email:"+Email
-//				+"<br> Gender:"+Gender
-//				+"<br> DOB:"+DOB
-//				+"<br> Country:"+country
-//				+"<br> Skills:"+Arrays.toString(skills)
-//				+"<br> Address:"+Address+"</h1>");
 		
 		student std = new student();
 		std.setAddress(req.getParameter("address"));
@@ -45,7 +25,6 @@ public class print extends GenericServlet{
 		std.setDOB(Date.valueOf(req.getParameter("dob")));
 		std.setEmail(req.getParameter("mail"));
 		std.setGender( req.getParameter("gender"));
-//		std.setId(id);
 		std.setMobileNo(Long.parseLong(req.getParameter("mobile")));
 		std.setName(req.getParameter("name"));
 		std.setSkills(req.getParameterValues("skills"));
